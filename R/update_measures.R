fc5854ef9ae05d8cb20bf144f9bf6a9db508d44b
nskaff <nskaff@comcast.net>
2021-04-26 14:29:28 -0400

    updating data load

--- R/update_measures.R
+++ R/update_measures.R
@@ -69,8 +69,12 @@ update_measures<-function(){
                               Meas_Inds_CAs_clean$indicator_ID,
                               Meas_Inds_CAs_clean$content_area_ID),]
   
+  assign("measures_indicators_CAs", 
+         measures_indicators_CAs, 
+         envir = .GlobalEnv)
+  
   save(measures_indicators_CAs,
-       file=paste(file.path(system.file(package=EPHTrackR), 
+       file=paste(file.path(system.file(package="EPHTrackR"), 
                             "data/measures_indicators_CAs.RData")))
        
   # save(measures_indicators_CAs,
